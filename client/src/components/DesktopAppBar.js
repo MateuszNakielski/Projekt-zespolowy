ab0dddbfe634ea8f3876292e3069b41f021efa35
Łukasz Wolak <lukasz.wolak@theluke.pl>
2018-05-26 00:29:19 +0200

    Minor spacing fixes

--- client/src/components/DesktopAppBar.js
+++ client/src/components/DesktopAppBar.js
@@ -8,6 +8,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Hidden from '@material-ui/core/Hidden';
 import Menu from './Menu'
+import { withRouter } from 'react-router-dom';
 
 const styles = theme => ({
   root: {
@@ -17,25 +18,29 @@ const styles = theme => ({
     maxWidth: theme.spacing.unit * 150,
     width: '100%'
   },
+  title: {
+    cursor: 'pointer',
+  },
   flex: {
     flex: 1,
   },
 });
 
 function DesktopAppBar(props) {
-  const { classes } = props;
+  const { history, classes } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="fixed" style={{alignItems: 'center'}}>
+      <AppBar position="fixed" style={{ alignItems: 'center' }}>
         <Toolbar className={classes.container}>
           <Hidden smUp>
             <IconButton className={classes.menuButton} aria-label="Menu">
               <MenuIcon />
             </IconButton>
           </Hidden>
-          <Typography variant="title" className={classes.flex}>
+          <Typography variant="title" className={classes.title} onClick={() => { history.push('/events') }}>
             PetAlert
           </Typography>
+          <div className={classes.flex} />
           <Hidden xsDown>
             <Menu />
           </Hidden>
@@ -49,4 +54,4 @@ DesktopAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(DesktopAppBar);
+export default withStyles(styles)(withRouter(DesktopAppBar));
--- client/src/components/Events/Comment.js
+++ client/src/components/Events/Comment.js
@@ -5,20 +5,23 @@ import moment from 'moment';
 
 moment.locale('pl');
 
-const styles = {
+const styles = theme => ({
   flex: {
     flex: 1,
   },
-};
+  item: {
+    paddingTop: theme.spacing.unit,
+  }
+});
 
 const Comment = (props) => {
   const { classes } = props;
   return (
     <Grid container direction="column" className={classes.flex}>
       <Divider/>
-      <Typography variant="body1">{props.comment}</Typography>
+      <Typography variant="body1" className={classes.item}>{props.comment}</Typography>
       <Grid container direction="row" alignItems="space-between">
-        <Grid item className={classes.flex}>
+        <Grid item className={[classes.flex, classes.item].join(' ')}>
           <Typography variant="caption">{getDisplayName(props.user)}</Typography>
         </Grid>
         <Grid item>
